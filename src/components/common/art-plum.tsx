e6218e5d56b9197ce4a668d001470f80a263f07f
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2026-03-17 17:00:57 +0800

    fix: adjust PlumCanvas div styling to remove bottom and right constraints

--- src/components/common/art-plum.tsx
+++ src/components/common/art-plum.tsx
@@ -132,7 +132,7 @@ function PlumCanvas() {
 
 	return (
 		<div
-			className="fixed top-0 bottom-0 left-0 right-0 pointer-events-none"
+			className="fixed top-0 left-0 pointer-events-none"
 			style={{
 				zIndex: -1,
 				maskImage: mask,
